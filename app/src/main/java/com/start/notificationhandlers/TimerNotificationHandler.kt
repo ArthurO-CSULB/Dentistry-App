d5718f683d85f2cf91ea319ec3fece9ce6c6154b
kelsonnguyen <kelsonnguyen007@gmail.com>
2025-03-08 21:12:06 -0800

    Reminders are now showing properly in the database and weekly calendar view, notifications are working for reminders created.

--- app/src/main/java/com/start/notificationhandlers/TimerNotificationHandler.kt
+++ app/src/main/java/com/start/notificationhandlers/TimerNotificationHandler.kt
@@ -1,20 +1,8 @@
 package com.start.notificationhandlers
-import android.annotation.SuppressLint
-import android.app.AlarmManager
 import android.app.NotificationManager
-import android.app.PendingIntent
-import android.content.BroadcastReceiver
 import android.content.Context
-import android.content.Intent
-import android.os.Build
-import android.util.Log
 import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
 import com.example.dentalhygiene.R
-import com.start.pages.NotificationReceiver
-import kotlinx.coroutines.delay
-import java.text.SimpleDateFormat
-import java.util.Locale
 import kotlin.random.Random
 
 /*
