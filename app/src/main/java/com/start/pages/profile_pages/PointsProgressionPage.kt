8db61c2fa1742f975932aaad23b77a0c748c3f75
gamvr <gabrielvillanueva11155@gmail.com>
2025-04-05 21:24:31 -0700

    Implemented temp variables in the pointsProgressionPage that store the value of prestige and exp before they were changed. Temp variables will be used to animated progress bar. Points is reactive to and progress bar is reactive to changes to the database. When points increase/decrease, text showing points above bar counts up/down.

--- app/src/main/java/com/start/pages/profile_pages/PointsProgressionPage.kt
+++ app/src/main/java/com/start/pages/profile_pages/PointsProgressionPage.kt
@@ -1,5 +1,7 @@
 package com.start.pages.profile_pages
 
+import android.util.Log
+import android.util.MutableInt
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
@@ -16,6 +18,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
@@ -30,10 +33,12 @@ import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.dentalhygiene.R
+import com.google.common.math.DoubleMath.roundToInt
 import com.start.viewmodels.PointsProgressionViewModel
 import com.start.viewmodels.Prestige
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlin.math.roundToInt
 
 @Composable
 fun PointsProgressionPage(modifier: Modifier, navController: NavController,
@@ -43,10 +48,15 @@ fun PointsProgressionPage(modifier: Modifier, navController: NavController,
     // Collect the flows of prestige and exp emitted from view model.
     val prestige = pointsProgressionViewModel.prestige.collectAsState()
     val exp = pointsProgressionViewModel.experience.collectAsState()
+    // Store the exp value from the previous recomposition of the UI. Be sure to update it when
+    // exp changes.
+    val expTemp = remember {mutableIntStateOf(exp.value.toInt())}
+    val prestigeTemp = remember {mutableIntStateOf(prestige.value.toInt())}
+
     val prestigeInfo: Prestige = pointsProgressionViewModel.prestiges[prestige.value.toInt()]
     // Get the max experience for the current prestige of the user from the array that
     // stores objects of prestiges.
-    val maxExp: Long = pointsProgressionViewModel.prestiges[prestige.value.toInt()].maxExp
+    val maxExp: Long = pointsProgressionViewModel.prestiges[prestigeTemp.intValue].maxExp
 
     // Variable to store the level progress of the progress bar. Will be updated iteratively
     // in a coroutine to display the progress increasing/decreasing. Initialize it with the initial
@@ -54,14 +64,40 @@ fun PointsProgressionPage(modifier: Modifier, navController: NavController,
     // recompositions.
     var levelProgress by remember {mutableStateOf(exp.value.toFloat() / maxExp.toInt())}
 
-    // Coroutine scope to launch the coroutine to update the progress bar.
+    // Coroutine scope to reset points and increase prestige.
     val scope = rememberCoroutineScope()
 
     // Boolean to store if the progress bar is moving or not.
     var progressMoving by remember {mutableStateOf(false)}
 
+    // Launched effect to update the progress bar reactively to the changes in database.
     LaunchedEffect(exp.value) {
 
+
+        // Previous percentage the bar was at based off user experience, vs the current
+        // percentage the bar is currently at when it increased/decreased.
+        val prevPercentage = ((expTemp.intValue.toFloat() / maxExp.toInt()) * 100).toInt()
+        val currPercentage = ((exp.value.toFloat() / maxExp.toInt()) * 100).toInt()
+
+        Log.d("PREV PERCENTAGE", prevPercentage.toString())
+        Log.d("CURR PERCENTAGE", currPercentage.toString())
+
+        // State that the progress bar is moving.
+        progressMoving = true
+
+        // Call back function to update the progress bar. Pass in integer values of percentage.
+        // So if you want 10% pass in 10 etc.
+        reactiveProgress(prevPercentage, currPercentage) { progress ->
+            // Update the progress bar from the value that is passed in.
+            levelProgress = progress
+        }
+
+        // When progression is finished, temp values become the current values.
+        expTemp.intValue = exp.value.toInt()
+        prestigeTemp.intValue = prestige.value.toInt()
+
+        // State that the progress bar is not moving.
+        progressMoving = false
     }
 
     Column (
@@ -115,18 +151,22 @@ fun PointsProgressionPage(modifier: Modifier, navController: NavController,
                         // the progress bar to decrease from the max to zero.
                         if (exp.value >= maxExp) {
                             // Boolean to store if the progress bar is moving or not. Disable.
-                            progressMoving = true
+                            // progressMoving = true
                             // Launch the coroutine that will reset the progress bar.
                             scope.launch {
+
+                                /*
                                 // Call resetProgress to reset the progress bar asynchonously.
                                 resetProgress { progress ->
                                     levelProgress = progress
                                 }
+                                 */
                                 // Increase the prestige of the user by one and reset the points to zero.
                                 pointsProgressionViewModel.prestige()
                                 pointsProgressionViewModel.resetPoints()
+                                //delay(2000)
                                 // Enable the progress bar again.
-                                progressMoving = false
+                                // progressMoving = false
                             }
                         }
                     },
@@ -138,7 +178,14 @@ fun PointsProgressionPage(modifier: Modifier, navController: NavController,
         }
 
 
-        Text("${exp.value.toInt()} / ${maxExp.toInt()}")
+
+        if (progressMoving) {
+            Text("${(maxExp.toInt() * levelProgress).roundToInt()} / ${maxExp.toInt()}")
+        } else {
+            Text("${exp.value.toInt()} / ${maxExp.toInt()}")
+        }
+
+        // Progress Indicator for the points bar.
         LinearProgressIndicator(
             progress = {levelProgress},
             modifier = modifier
@@ -214,6 +261,47 @@ suspend fun resetProgress(updateProgress: (Float) -> Unit) {
         // resetting.
         currDelay = maxDelay - ((maxDelay - minDelay) * progress).toLong()
     }
+}
+// Method that when the exp value changes, will update the progress bar
+suspend fun reactiveProgress(prevPercent: Int = 0, currPercent: Int = 0, updateProgress: (Float) -> Unit) {
+    // max delay and min delay
+    val maxDelay = 15L
+    val minDelay = 5L
+    // Variable to store the delay after each iteration. Will get smaller as progress increases.
+    var currDelay = maxDelay
+    // Iterate from high to low if the previous percentage is greater than the current percentage.
+    if (prevPercent > currPercent) {
+        for (i in prevPercent downTo currPercent) {
+            updateProgress(i.toFloat() / 100)
+            // Reference for progress and bar acceleration: ChatGPT
+            // Progress of how finished the bar animation is completed. As progress increases,
+            // currDelay decreases.
+            // Calculate distance of i from prevPercent over the distance of prevPercent to currPercent.
+            val progress = (prevPercent - i).toFloat() / (prevPercent - currPercent)
+            delay(currDelay)
+            // Accelerate the progress bar. Delay will decrease as the progress bar is almost done
+            // resetting.
+            currDelay = maxDelay - ((maxDelay - minDelay) * progress).toLong()
+        }
+
+    }
+    // Iterate from low to high if the previous percentage is less than the current percentage.
+    if (prevPercent < currPercent) {
+        for (i in prevPercent..currPercent) {
+            updateProgress(i.toFloat() / 100)
+
+            // Reference for progress and bar acceleration: ChatGPT
+            // Progress of how finished the bar animation is completed. As progress increases,
+            // currDelay decreases.
+            // Calculate the distance of i from prevPercent over the distance of prevPercent to currPercent.
+            val progress = (i - prevPercent).toFloat() / (currPercent - prevPercent)
+            delay(currDelay)
+            // Accelerate the progress bar. Delay will decrease as the progress bar is almost done
+            // resetting.
+            currDelay = maxDelay - ((maxDelay - minDelay) * progress).toLong()
+        }
+    }
+
 }
 
 
