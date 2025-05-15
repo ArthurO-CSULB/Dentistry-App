c94aa7c2ee8f2f139fee1e4cee7e2021450702d1
gamvr <gabrielvillanueva11155@gmail.com>
2025-05-15 03:14:18 -0700

    clean up UI leaderboard

--- app/src/main/java/com/start/pages/profile_pages/LeaderboardsStatsPage.kt
+++ app/src/main/java/com/start/pages/profile_pages/LeaderboardsStatsPage.kt
@@ -1,12 +1,23 @@
 package com.start.pages.profile_pages
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -19,19 +30,45 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.start.viewmodels.LeaderboardsStatsViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LeaderboardsStatsPage(modifier: Modifier, navController: NavController,
                           leaderBoardStatsViewModel: LeaderboardsStatsViewModel) {
     Column(
         modifier = modifier
+            .background(MaterialTheme.colorScheme.primary)
             .fillMaxSize(),
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        // Top Bar to go back to the profile page
+        TopAppBar(
+            title =
+            {
+                Text(
+                    text = "Back to Profile Page",
+                    color = MaterialTheme.colorScheme.primary,
+                    fontWeight = FontWeight.SemiBold
+                )
+            },
+            modifier = Modifier
+                .fillMaxWidth(),
+            navigationIcon = {
+                IconButton(onClick = {navController.popBackStack()}) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = "Back Button",
+                        tint = MaterialTheme.colorScheme.primary
+                    )
+                }
+            },
+            colors = TopAppBarDefaults. topAppBarColors(MaterialTheme.colorScheme.secondaryContainer)
+        )
+
         // Title of Page
         Box(
             modifier = modifier
-                .border(1.dp, Color.Black)
+                //.border(1.dp, Color.Black)
                 .padding(horizontal = 8.dp, vertical = 16.dp)
         ) {
             Text(
@@ -39,22 +76,54 @@ fun LeaderboardsStatsPage(modifier: Modifier, navController: NavController,
                 fontSize = 48.sp,
                 fontWeight = FontWeight.Bold,
                 textAlign = TextAlign.Center,
-                lineHeight = 1.5.em
+                lineHeight = 1.5.em,
+                color = MaterialTheme.colorScheme.secondaryContainer
             )
         }
 
         // List of stats
         Column(
             modifier = modifier
-                .border(1.dp, Color.Black)
-                .weight(2f)
+                .border(
+                    1.dp,
+                    Color.Black,
+                    shape = RoundedCornerShape(16.dp)
+                )
+                .weight(2f),
+            horizontalAlignment = Alignment.CenterHorizontally
+
         ) {
             // TODO: use the viewModel to get the status, then display some here Add more in the future.
-            Text("User with the most points: ...")
-            Text("The most consistent tooth brusher: ...")
-            Text("The most liked dentist: ...")
-            Text("Highest score on flappy tooth: ...")
-            Text("The smartest dental scholar (deals with trivia): ...")
+            Text(
+                "User with the most points:\n______",
+                textAlign = TextAlign.Center,
+                lineHeight = 2.em,
+                color = MaterialTheme.colorScheme.secondaryContainer,
+            );
+            Text(
+                "The most consistent tooth brusher:\n______", textAlign =
+                TextAlign.Center,
+                lineHeight = 2.em,
+                color = MaterialTheme.colorScheme.secondaryContainer
+            )
+            Text(
+                "The most liked dentist:\n______",
+                textAlign = TextAlign.Center,
+                lineHeight = 2.em,
+                color = MaterialTheme.colorScheme.secondaryContainer
+            )
+            Text(
+                "Highest score on flappy tooth:\n______",
+                textAlign = TextAlign.Center,
+                lineHeight = 2.em,
+                color = MaterialTheme.colorScheme.secondaryContainer
+            )
+            Text(
+                "The smartest dental scholar (deals with trivia):\n______",
+                textAlign = TextAlign.Center,
+                lineHeight = 2.em,
+                color = MaterialTheme.colorScheme.secondaryContainer
+            )
         }
 
 
